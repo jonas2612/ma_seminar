b0a4b3d3caa2d540583b62ffc1a6c929eb9338e7
jonas2612 <ge49zen@mytum.de>
2026-09-06 07:49:59 +0200

    Update tester for DEA

--- test_download_and_extract.py
+++ test_download_and_extract.py
@@ -12,27 +12,30 @@ logger = logging.getLogger("shape_mapping")
 
 
 gse_number = "GSE253903"
-base_dir = Path(r"C:\Users\jonas\OneDrive\Desktop\Semester_4_Bio\Masterpraktikum\test_data")
+base_dir = Path(r"/usr/local/storage/data_scRNAseq")
 #process_gse(gse_number, base_dir / "raw_data_download", base_dir / "raw_data")
 
 
-gse_number = "GSE159677"
-#process_gse(gse_number, base_dir / "raw_data_download", base_dir / "raw_data")
-#data_dirs = determine_level(base_dir / "raw_data" / gse_number)
+gse_number = "GSE253903"
+process_gse(gse_number, base_dir / "raw_data_download", base_dir / "raw_data", logger)
+data_dir = base_dir / "raw_data" / gse_number
+data_dirs = determine_level(data_dir, logger)
 adatas = []
-metadata = pd.read_csv(r"C:\Users\jonas\OneDrive\Desktop\Semester_4_Bio\Masterpraktikum\metadata_all_samples.txt", sep = "\t")
-parent = Path(r"C:\Users\jonas\OneDrive\Desktop\Semester_4_Bio\Masterpraktikum\test_data\raw_data\GSE159677")
-for path in tqdm([x for x in listdir(parent) if x.endswith("matrix.mtx.gz") and "GSM" in x]):
+metadata = pd.read_csv(r"/home/f/flor/metadata_all_samples.txt", sep = "\t")
+for path in tqdm([x for x in listdir(data_dir) if x.endswith("matrix.mtx.gz") and "GSM" in x]):
     logger.info(f"Working on {path}")
-    adata = read_raw_data(parent / path, metadata, logger)
+    adata = read_raw_data(data_dir / path, metadata, logger)
     adata = qc_statistical(adata, logger)
-    #adata = doublet_detection(adata, logger)
+    adata = doublet_detection(adata, logger)
     adatas.append(adata)
 adata = combine_samples(adatas, logger)
 adata = normalize_data(adata, logger)
 adata = pca(adata, logger)
 adata = neighbors(adata, logger)
-adata = umap(adata, logger, save_path_plot = r"C:\Users\jonas\OneDrive\Desktop\Semester_4_Bio\Masterpraktikum\test_umap.png")
-adata = annotate_adata(adata, logger, r"C:\Users\jonas\OneDrive\Desktop\gitrepos\plaque-atlas-mapping\automatic_mapping_level1.py",
-                       save_path_plot = r"C:\Users\jonas\OneDrive\Desktop\Semester_4_Bio\Masterpraktikum\test_umap_annot.png")
+adata = umap(adata, logger, save_path_plot = r"/home/f/flor/test_umap.png")
+adata = annotate_adata(adata, logger, "/home/f/flor/gitrepos/plaque-atlas-mapping/automatic_mapping_level1.py",
+                       save_path_plot = r"/home/f/flor/test_umap_annot.png", save_path=f"/home/f/flor/{gse_number}_annot.h5ad")
 print(adata)
+print(run_limma_dea(adata, "ECs", ("TRUE", "FALSE"), "ECs_atherosclerosis.tsv", 
+                    condition_col="symptomatic_atherosclerosis", logger=logger,
+                     cell_type_col="cell_type_level1"))
